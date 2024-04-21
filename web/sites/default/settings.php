66f0e477ca65c94c1fc28bdc7bbcc0985bdec503
Yevhen Korniievskyi <y.korniievskyi@dev-branch.com>
2024-04-21 17:29:15 +0300

    Made changes to the basic site configuration. Updated setting.php file: the pantheon settings now the latest, use config directory in settings php.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -6,17 +6,6 @@
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
- * Include the Pantheon-specific settings file.
- *
- * n.b. The settings.pantheon.php file makes some changes
- *      that affect all environments that this site
- *      exists in.  Always include this file, even in
- *      a local development environment, to ensure that
- *      the site settings remain consistent.
- */
-include __DIR__ . "/settings.pantheon.php";
-
-/**
  * Skipping permissions hardening will make scaffolding
  * work better, but will also raise a warning when you
  * install Drupal.
@@ -24,6 +13,8 @@ include __DIR__ . "/settings.pantheon.php";
  * https://www.drupal.org/project/drupal/issues/3091285
  */
 // $settings['skip_permissions_hardening'] = TRUE;
+
+$settings['config_sync_directory'] = '../config';
 
 /**
  * If there is a local settings file, then include it
@@ -38,3 +29,14 @@ $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
   require $ddev_settings;
 }
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all environments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
